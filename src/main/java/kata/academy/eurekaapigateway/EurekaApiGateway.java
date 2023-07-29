fcd86a36604596a33dd8575f80aa04b51aa967ba
Denis4230 <92786441+Denis4230@users.noreply.github.com>
2023-07-29 13:31:30 +0700

    Initial commit

---
+++ src/main/java/kata/academy/eurekaapigateway/EurekaApiGateway.java
@@ -0,0 +1,12 @@
+package kata.academy.eurekaapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//1234
+@SpringBootApplication
+public class EurekaApiGateway {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApiGateway.class, args);
+    }
+}
