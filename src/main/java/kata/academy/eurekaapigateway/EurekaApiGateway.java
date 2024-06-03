fd1918df45b2ae57cfe6d52da8da0175fd48d7bf
d.semenov <d.semenov@cloudcom.ru>
2024-06-03 17:46:39 +0300

    qqqq

--- src/main/java/kata/academy/eurekaapigateway/EurekaApiGateway.java
+++ src/main/java/kata/academy/eurekaapigateway/EurekaApiGateway.java
@@ -1,5 +1,7 @@
 package kata.academy.eurekaapigateway;
 
+import javax.xml.crypto.Data;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //1234
@@ -9,8 +11,8 @@ public class EApiGateway {
     public static void main(String[] args) {
         SpringApplication.run(EApiGateway.class, args);
     }
-    void lds(Throwable throwable, Integer integer) {
-        System.out.println(throwable);
+    void lds(Data data, String s) {
+        System.out.println(s);
     }
 }
 //opsidjfoijsopfihso
